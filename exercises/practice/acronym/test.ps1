63611a4b314c4ca2e24cba5869497db1cd902b79
ManfredHeider <47299517+ManfredHeider@users.noreply.github.com>
2024-07-07 16:23:41 +0200

    Update test.ps1
    Write-Output "MH PSScriptRoot -> $PSScriptRoot"

--- exercises/practice/acronym/test.ps1
+++ exercises/practice/acronym/test.ps1
@@ -12,11 +12,10 @@ if ($cobolcheckCmd) {
 
 Write-Output "Run cobolcheck."
 Set-Location $PSScriptRoot
+Write-Output "MH PSScriptRoot -> $PSScriptRoot"
 
 Invoke-Expression "$cobolcheck -p $slug"
-#Invoke-Expression "cobc -xj test.cob"
-Invoke-Expression "mhcobc -xj -I${PSScriptRoot}\src test.cob"
-
+Invoke-Expression "cobc -xj test.cob"
 
 if ($Lastexitcode -ne 0) {
   exit $Lastexitcode
