b2372da39b116a5fb051f250de8505b0386b7ff7
ManfredHeider <47299517+ManfredHeider@users.noreply.github.com>
2024-07-20 16:01:39 +0200

    Update test.ps1
    write-output

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,8 +1,12 @@
 function verify_exercise ($practiceExerciseDir) {
     $dir=[System.IO.Path]::GetFullPath($practiceExerciseDir)
+    Write-Output "MH dir -> $dir"
     $slug=Split-Path $dir -Leaf
+    Write-Output "MH slug -> $slug"
     $implementationFile="$dir/.meta/proof.ci.cob"
+    Write-Output "MH implementationFile -> $implementationFile"
     $stubFile="$dir/src/$slug.cob"
+    Write-Output "MH stubFile -> $stubFile"
     $stubBackupFile="$stubFile.bak"
     Copy-Item $stubFile -Destination $stubBackupFile
     Copy-Item $implementationFile -Destination $stubFile
